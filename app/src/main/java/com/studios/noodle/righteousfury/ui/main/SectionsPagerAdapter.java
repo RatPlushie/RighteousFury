df07bed17c5742dcee26e3d61a005fc88c2d6892
aki <kieran.mortimer@gmail.com>
2019-06-13 12:02:23 +0200

    Initial commit

---
+++ app/src/main/java/com/studios/noodle/righteousfury/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,58 @@
+package com.studios.noodle.righteousfury.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.studios.noodle.righteousfury.R;
+import com.studios.noodle.righteousfury.Tab1_Character;
+import com.studios.noodle.righteousfury.Tab2_Inventory;
+import com.studios.noodle.righteousfury.Tab3_Attack;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3}; // add more arguments here, per tab required
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                Tab1_Character tab1_character = new Tab1_Character();
+                return tab1_character;
+            case 1:
+                Tab2_Inventory tab2_inventory = new Tab2_Inventory();
+                return tab2_inventory;
+            case 2:
+                Tab3_Attack tab3_attack = new Tab3_Attack();
+                return tab3_attack;
+        }
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show total pages. Increase Integer value to the amount of tabs you want
+        return 3;
+    }
+}
