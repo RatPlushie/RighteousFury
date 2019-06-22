48a6d6b1e9ed8e7d249b56d2c508fe8694b4fd2d
Aki <kieran.mortimer@gmail.com>
2019-06-22 15:30:22 +0200

    Attribute roll page layout updated
    - Implemented a placeholder design of the attribute roll page.
    - Added logic for the roll modifier seekbar.
    - Added logic for the roll threshold text view
    - Added more string definitions

--- app/src/main/java/com/studios/noodle/righteousfury/AttributeRollActivity.java
+++ app/src/main/java/com/studios/noodle/righteousfury/AttributeRollActivity.java
@@ -1,19 +1,68 @@
 package com.studios.noodle.righteousfury;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import static com.studios.noodle.righteousfury.tab1Character.SHARED_PREFS;
+
 public class AttributeRollActivity extends AppCompatActivity {
 
-    TextView activityTile;
-    TextView activityAttributeVal;
+    // AttributeRollActivity Variables
+    private TextView activityTile;
+    private TextView activityAttributeVal;
+    private TextView modifierValue;
+    private TextView currentFatePointValue;
+    private TextView rollRequiredVal;
+    private TextView rollOutcome;
+    private TextView degreeOfOutcome;
+
+    private Button rollButton;
+    private Button reRollButton;
+    private Button fatepointRollButton;
+
+    private SeekBar modifierSeekBar;
+
+    private ImageView attributeImageView;
+
+    private int currentFP;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attribute_roll);
+
+        // Initialisation of the activity's objects
+        activityTile                = findViewById(R.id.AttributeTitleTextView);
+        activityAttributeVal        = findViewById(R.id.AttributeValTextView);
+        modifierValue               = findViewById(R.id.ModifierValTextView);
+        currentFatePointValue       = findViewById(R.id.currentFatePointTotalTextView);
+        rollRequiredVal             = findViewById(R.id.rollThresholdValTextView);
+        rollOutcome                 = findViewById(R.id.rollOutcomeValTextView);
+        degreeOfOutcome             = findViewById(R.id.degreeOutcomeVal);
+        rollButton                  = findViewById(R.id.rollButton);
+        reRollButton                = findViewById(R.id.reRollButton);
+        fatepointRollButton         = findViewById(R.id.fatepointRollButton);
+        modifierSeekBar             = findViewById(R.id.rollModifierSeekBar);
+        attributeImageView          = findViewById(R.id.attributeImageView);
+
+
+        // Initialisation of the shared preference object to load character data onResume
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+
+        // Retrieving the amount of fate points the user has and parsing it into an integer
+        currentFP = Integer.parseInt(sharedPreferences.getString("character_current_fp", ""));
+
+        // Setting the starting amount of fatepoints the user has
+        currentFatePointValue.setText("[ " + currentFP + " ]");
+
 
         // Retrieve the bundle
         Bundle bundle = getIntent().getExtras();
@@ -23,16 +72,59 @@ public class AttributeRollActivity extends AppCompatActivity {
         String attributeName = bundle.getString("attributeName");
 
         // Extracting the value of the attribute as an integer
-        int attributeVal = Integer.parseInt(bundle.getString("attributeValue"));
+        final int attributeVal = Integer.parseInt(bundle.getString("attributeValue"));
+
 
         // Setting the title of the roll page to the attribute's name
-        activityTile = findViewById(R.id.AttributeTitleTextView);
         activityTile.setText(attributeName);
 
         // Setting the value of the attribute visually
-        activityAttributeVal = findViewById(R.id.AttributeValTextView);
         activityAttributeVal.setText(Integer.toString(attributeVal));
 
+
+        // Initially setting the roll threshold to the base stat - will be modified on seekbar change afterwards
+        rollRequiredVal.setText(bundle.getString("attributeValue"));
+
+        // Call for the seekbar's behaviour
+        modifierSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                // sets the Modifier value text view above the seekbar
+                modifierValue.setText(Integer.toString(i - 100));
+
+                // Sets the roll threshold text view
+                rollRequiredVal.setText(Integer.toString(attributeVal + (i - 100)));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Initialisation of the shared preferences object
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+
+        // Initialisation of the shared preferences editor
+        SharedPreferences.Editor edit = sharedPreferences.edit();
+
+        // Updates the amount of FP points the character currently has to the shared preferences
+        edit.putString("character_current_fp", Integer.toString(currentFP));
+
+        // Applies the changes to the shared preferences file
+        edit.apply();
     }
 
     @Override
